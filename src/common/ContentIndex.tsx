a3838b7a58868143ff8429b1b4f957357ae28427
Le Huynh Duc <duclh@hybrid-technologies.vn>
2021-11-20 22:47:21 +0700

    add more css

--- src/common/ContentIndex.tsx
+++ src/common/ContentIndex.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function ContentIndex() {
   return (
-    <div className="fixed  top-[12.5rem] w-[14vw] h-[100vh]">
+    <div className="fixed  top-[13.4rem] w-[14vw] h-[100vh]">
       <div className="text-gray-700 pb-3 text-2xl font-semibold">Topic</div>
       <div className="flex flex-col">
         <ul className="contentIndex w-full"></ul>
