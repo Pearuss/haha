e719bc758c7cb703e9b3714ddda17bc3514d892e
Le Huynh Duc <duclh@hybrid-technologies.vn>
2021-11-21 16:24:40 +0700

    add more css

--- src/common/FilterMyPosts.tsx
+++ src/common/FilterMyPosts.tsx
@@ -24,20 +24,20 @@ export function FilterMyPosts({ onClickNoFilter, onClickFilter, setFilter }: any
   };
 
   return (
-    <div className="w-full bg-gray-500 flex py-4 px-2 items-center justify-around">
-      <p className="text-black text-2xl font-semibold xl:text-xl lg:text-lg">Filter Posts:</p>
-      <div className="flex">
+    <div className="w-full bg-gray-400 flex py-3 px-2 items-center justify-around text-gray-700 rounded-md ">
+      <p className="text-2xl font-semibold xl:text-xl lg:text-lg">Filter Posts:</p>
+      <div className="flex items-center">
         <p className="font-normal text-lg mr-2 xl:text-base lg:text-sm md:text-sm">Category:</p>
-        <select className="category cursor-pointer w-44 xl:w-28 lg:w-20 md:w-16">
+        <select className="category cursor-pointer w-36 xl:w-28 lg:w-20 md:w-16 rounded-md py-1 px-3">
           {catData?.map((item: any) => (
             <option key={item.id}>{item.name}</option>
           ))}
         </select>
       </div>
-      <div className="flex">
+      <div className="flex items-center">
         <p className="font-normal text-lg mr-2 xl:text-base lg:text-sm md:text-sm">Tag:</p>
         <select
-          className="category cursor-pointer w-44 xl:w-28 lg:w-20 md:w-16 "
+          className="category cursor-pointer w-36 xl:w-28 lg:w-20 md:w-16 rounded-md py-1 px-3 "
           onChange={(e) => setTagName(e.target.value)}
         >
           {tagData?.followingTags?.map((item: any) => (
@@ -47,13 +47,13 @@ export function FilterMyPosts({ onClickNoFilter, onClickFilter, setFilter }: any
       </div>
       <div className="flex">
         <button
-          className="mr-2 float-right rounded-3xl px-3 py-1 bg-grayLightText hover:opacity-70"
+          className="mr-4 float-right rounded-md px-3 py-1 shadow-sm bg-grayLightText hover:opacity-70"
           onClick={handleFilter}
         >
-          Go
+          Search
         </button>
         <button
-          className="mr-2 float-right rounded-3xl px-3 py-1 bg-white hover:opacity-70 shadow-2xl drop-shadow-2xl"
+          className="mr-2 float-right rounded-md px-3 py-1 bg-white hover:opacity-70  drop-shadow-2xl"
           onClick={handleNoFilter}
         >
           Cancel
