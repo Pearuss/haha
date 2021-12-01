c074d2f89714c31a26c320aadfbf94b3a09a9091
Diem Nguyen The <diemnt@hybrid-technologies.vn>
2021-12-01 11:17:38 +0700

    fix ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,9 +41,24 @@ function HomePage({ data }: any) {
 
   return (
     <div className="mr-16 md:mr-0 sm:mr-0 ssm:mx-auto ssm:px-[2vw]">
-      <p className="text-5xl 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-[40px] sm:text-[40px] ssm:text-3xl pb-6 text-black font-normal">
+      {/* <p className="text-5xl 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-[40px] sm:text-[40px] ssm:text-3xl pb-6 text-black font-normal">
         Create diagrams online realtime collaboration!
-      </p>
+      </p> */}
+      <div className="relative w-full h-44 bg-white p-4 rounded-md shadow-md">
+        <p className="text-5xl 2xl:text-4xl xl:text-3xl lg:text-2xl md:text-[40px] sm:text-[40px] ssm:text-3xl pb-6 text-black font-normal cursor-pointer">
+          Sunt aut facere repellat provident occaecati
+        </p>
+        <div className="text-sm">
+          Quia et suscipit suscipit recusandae consequuntur consequuntur consequuntur consequuntur
+          ....
+        </div>
+
+        <div className="absolute bottom-1 right-4 pb-2 text-xs">
+          <span>ReactJs - Pearuss</span>
+          <span className="ml-3">|</span>
+          <span className="ml-3">12 Feb 2020 </span>
+        </div>
+      </div>
       <div className="mt-4 text-gray-900">
         <div className="grid grid-cols-3 lg:grid-cols-2 xl:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 ssm:grid-cols-1 sm:h-[10.5rem] xl:h-[10.5rem] h-40 gap-10 pt-4 mb-10 ssm:mx-auto">
           <div className="relative w-full h-full bg-white p-4 rounded-md shadow-md cursor-pointer hover:transform hover:shadow-custom hover:scale-105 transition-all">
@@ -61,9 +76,7 @@ function HomePage({ data }: any) {
           </div>
           <div className="relative w-full h-full bg-white p-4 rounded-md shadow-md cursor-pointer hover:transform hover:shadow-custom hover:scale-105 transition-all ssm:hidden">
             <div className="font-medium  pb-1 text-black">Sunt aut facere repellat provident</div>
-            <div className="text-sm">
-              Quia et suscipit suscipit recusandae consequuntur ....
-            </div>
+            <div className="text-sm">Quia et suscipit suscipit recusandae consequuntur ....</div>
 
             <div className="absolute bottom-1 right-4 text-gray-700 pb-2 text-xs">
               <span>ReactJs - Pearuss</span>
@@ -73,9 +86,7 @@ function HomePage({ data }: any) {
           </div>
           <div className="relative w-full h-full bg-white p-4 rounded-md shadow-md cursor-pointer hover:transform hover:shadow-custom hover:scale-105 transition-all lg:hidden md:hidden xl:hidden sm:hidden ssm:hidden">
             <div className="font-medium  pb-1 text-black">Sunt aut facere repellat provident</div>
-            <div className="text-sm">
-              Quia et suscipit suscipit recusandae consequuntur ....
-            </div>
+            <div className="text-sm">Quia et suscipit suscipit recusandae consequuntur ....</div>
 
             <div className="absolute bottom-1 right-4 text-gray-700 pb-2 text-xs">
               <span>ReactJs - Pearuss</span>
