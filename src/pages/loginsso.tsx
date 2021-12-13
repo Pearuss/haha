b7829df10d686f44564645bb89cb13078ed0ec30
Le Huynh Duc <duclh@hybrid-technologies.vn>
2021-12-13 15:54:33 +0700

    add login sso

---
+++ src/pages/loginsso.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from 'react'
+
+interface Props {
+    
+}
+
+function loginsso({}: Props): ReactElement {
+    return (
+        <div>
+            dasdas
+        </div>
+    )
+}
+
+export default loginsso
