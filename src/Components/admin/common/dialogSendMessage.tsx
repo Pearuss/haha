4f6772ee629831fbad2185eaac07486d7feca90a
Le Huynh Duc <duclh@hybrid-technologies.vn>
2021-12-15 15:51:37 +0700

    fix ui admin

--- src/Components/admin/common/dialogSendMessage.tsx
+++ src/Components/admin/common/dialogSendMessage.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -9,9 +10,9 @@ import TextField from '@mui/material/TextField';
 export default function AlertDialog() {
   const [open, setOpen] = React.useState(true);
 
-//   const handleClickOpen = () => {
-//     setOpen(true);
-//   };
+  //   const handleClickOpen = () => {
+  //     setOpen(true);
+  //   };
 
   const handleClose = () => {
     setOpen(false);
@@ -19,9 +20,6 @@ export default function AlertDialog() {
 
   return (
     <div>
-      {/* <Button variant="outlined" onClick={handleClickOpen}>
-        Open alert dialog
-      </Button> */}
       <Dialog
         open={open}
         onClose={handleClose}
