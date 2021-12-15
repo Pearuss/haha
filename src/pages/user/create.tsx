6f5ab63724dec548a29e07009acc7627e936679f
Diem Nguyen The <diemnt@hybrid-technologies.vn>
2021-12-15 17:54:56 +0700

    fix conflict

--- src/pages/user/create.tsx
+++ src/pages/user/create.tsx
@@ -42,7 +42,6 @@ function UserCreatePage() {
     image: '',
     public: true,
   });
-<<<<<<< HEAD
 
   useEffect(() => {
     const button = document.createElement('button');
@@ -85,8 +84,6 @@ function UserCreatePage() {
     e.preventDefault();
     document.getElementById('uploadImgMD')?.click();
   }
-=======
->>>>>>> 9e15705fb2c8095645c18ac212cfeaa505cbe17b
 
   const changeTitle = (e: any) => {
     setNewPost((state: any) => ({ ...state, title: e.target.value }));
