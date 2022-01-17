cff5cdddb8fae5130a53bb675b2a3e1d19b4539d
duclh <duclh@hybrid-technologies.vn>
2022-01-17 11:33:20 +0700

    fix UI

--- src/pages/loginsso.tsx
+++ src/pages/loginsso.tsx
@@ -6,8 +6,6 @@ export default function LoginSSOPage() {
   const router = useRouter();
   useEffect(() => {
     if (router.query.session_state && router.query.code) {
-      console.log(router.query.code.toString());
-
       localStorage.setItem('tokenSso', router.query.code.toString());
       router.push('/');
     }
