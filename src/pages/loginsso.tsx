aa8b4aa7790a320cf721b02cc6e65c1b6140c2bf
duclh <duclh@hybrid-technologies.vn>
2022-02-08 12:02:04 +0700

    login sso

--- src/pages/loginsso.tsx
+++ src/pages/loginsso.tsx
@@ -1,36 +1,37 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+// import Backdrop from '@mui/material/Backdrop';
+// import useFetch from '../hooks/use-fetch';
+// import CircularProgress from '@mui/material/CircularProgress';
 import { useRouter } from 'next/router';
-import Swal from 'sweetalert2';
-
-import useFetch from '../hooks/use-fetch';
+// import Swal from 'sweetalert2';
 
 export default function LoginSSOPage() {
-  const [open, setOpen] = useState(true);
-  const handleClose = () => {
-    setOpen(false);
-  };
+  // const [open, setOpen] = useState(true);
+  // const handleClose = () => {
+  //   setOpen(false);
+  // };
   const router = useRouter();
   const token = router.query.code;
   console.log(token);
-  useEffect(() => {
-    Swal.fire('Login successfully but the feature is under maintenance.');
-  }, []);
+  // useEffect(() => {
+  //   Swal.fire('Login successfully but the feature is under maintenance.');
+  // }, []);
 
   useEffect(() => {
     const getToken = async () => {
-      const dataLogin = await useFetch('/api/sso-login', {
+      const res = await fetch('/api/sso-login', {
         method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           code: token,
         }),
       });
-
-      if (dataLogin.message.toString() === '200') {
-        router.replace('/');
-        console.log('success');
+      if (res.ok) {
+        const dataLogin = await res.json();
+        if (dataLogin.message.toString() === '200') {
+          router.replace('/');
+        }
       }
     };
     if (router.query.session_state && token) {
@@ -40,12 +41,13 @@ export default function LoginSSOPage() {
     // console.log(router.query.code);
   }, [token]);
   return (
-    <Backdrop
-      sx={{ color: '#fff', zIndex: (theme: any) => theme.zIndex.drawer + 1 }}
-      open={open}
-      onClick={handleClose}
-    >
-      <CircularProgress color="inherit" />
-    </Backdrop>
+    // <Backdrop
+    //   sx={{ color: '#fff', zIndex: (theme: any) => theme.zIndex.drawer + 1 }}
+    //   open={open}
+    //   onClick={handleClose}
+    // >
+    //   <CircularProgress color="inherit" />
+    // </Backdrop>
+    <div />
   );
 }
