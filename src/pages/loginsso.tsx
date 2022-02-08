d9080110f76741d9b5be1f948a5344834541769d
duclh <duclh@hybrid-technologies.vn>
2022-02-08 11:13:47 +0700

    login sso

--- src/pages/loginsso.tsx
+++ src/pages/loginsso.tsx
@@ -5,6 +5,8 @@ import CircularProgress from '@mui/material/CircularProgress';
 import { useRouter } from 'next/router';
 import Swal from 'sweetalert2';
 
+import useFetch from '../hooks/use-fetch';
+
 export default function LoginSSOPage() {
   const [open, setOpen] = useState(true);
   const handleClose = () => {
@@ -17,57 +19,26 @@ export default function LoginSSOPage() {
     Swal.fire('Login successfully but the feature is under maintenance.');
   }, []);
 
-  // useEffect(() => {
-  //   const getToken = async () => {
-  //     const formElement: any = {
-  //       code: router.query.code,
-  //       client_id: 'hybrid',
-  //       grant_type: 'authorization_code',
-  //       redirect_uri: 'http://localhost:9500/loginsso',
-  //     };
+  useEffect(() => {
+    const getToken = async () => {
+      const dataLogin = await useFetch('/api/sso-login', {
+        method: 'POST',
+        body: JSON.stringify({
+          code: token,
+        }),
+      });
 
-  //     let formBody: any = [];
-  //     for (let property in formElement) {
-  //       const encodedKey: any = encodeURIComponent(property);
-  //       const encodedValue: any = encodeURIComponent(formElement[property]);
-  //       formBody.push(encodedKey + '=' + encodedValue);
-  //     }
-  //     formBody = formBody.join('&');
-  //     const res = await fetch(
-  //       'http://localhost:8080/auth/realms/master/protocol/openid-connect/token',
-  //       {
-  //         method: 'POST',
-  //         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-  //         body: formBody,
-  //       }
-  //     );
-  //     console.log(res);
+      if (dataLogin.message.toString() === '200') {
+        router.replace('/');
+        console.log('success');
+      }
+    };
+    if (router.query.session_state && token) {
+      getToken();
+    }
 
-  //     if (res.ok) {
-  //       const data = await res.json();
-  //       console.log(data.access_token);
-
-  //       const resData = await fetch('/api/sso-login', {
-  //         method: 'POST',
-  //         headers: { 'Content-Type': 'application/json' },
-  //         body: JSON.stringify({
-  //           token: data.access_token,
-  //         }),
-  //       });
-  //       if (resData.ok) {
-  //         const dataLogin = await resData.json();
-  //         if (dataLogin.message.toString() === '200') {
-  //           router.replace('/');
-  //         }
-  //       }
-  //     }
-  //   };
-  //   if (router.query.session_state && token) {
-  //     getToken();
-  //   }
-
-  //   // console.log(router.query.code);
-  // }, [token]);
+    // console.log(router.query.code);
+  }, [token]);
   return (
     <Backdrop
       sx={{ color: '#fff', zIndex: (theme: any) => theme.zIndex.drawer + 1 }}
