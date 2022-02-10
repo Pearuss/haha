c46c636ca7144e99107db89275e856f5c8b9ce88
duclh <duclh@hybrid-technologies.vn>
2022-02-10 16:17:29 +0700

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,6 +12,11 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            rel="shortcut icon"
+            href={`${process.env.NEXT_PUBLIC_IMAGE_URL}/uploads/static/images/favicon.ico`}
+          />
+
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         </Head>
         <body>
